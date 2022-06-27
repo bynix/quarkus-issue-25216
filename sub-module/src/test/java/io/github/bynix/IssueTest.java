909a02ba745c02d722959e075288cd91667cd0c3
bynix <bynix@pm.me>
2022-06-27 11:52:31 +0800

    first commit
    Signed-off-by: bynix <bynix@pm.me>

---
+++ sub-module/src/test/java/io/github/bynix/IssueTest.java
@@ -0,0 +1,16 @@
+package io.github.bynix;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author bynix
+ */
+@QuarkusTest
+public class IssueTest {
+
+    @Test
+    public void issue() {
+        System.out.println(1);
+    }
+}
